edd6986c7ecfa934c0a38c29bf27280a775fe8ca
ctownsle <ctownsle@oswego.edu>
2019-05-07 19:37:23 -0400

    add constants for encryption algorithms

--- src/main/java/Misc/Constants.java
+++ src/main/java/Misc/Constants.java
@@ -7,6 +7,8 @@ public class Constants {
     public static final int UNICAST_PORT = 2709;
     public static final int MULTICAST_PORT = 2807;
     public static final int BUFFER_SIZE = 1024;
+    public static final String ALGORITHM = "AES";
+    public static final String EXT_ALGORITHM = "AES/ECB/PKCS5Padding";
 
     public static final int IMAGE_CHUNK_SIZE = BUFFER_SIZE + Integer.BYTES + (2 * Short.BYTES);
 
