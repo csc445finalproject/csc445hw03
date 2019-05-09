798eee1855409628c4880af18681f2bbebee6fbe
christian <csumano@oswego.edu>
2019-05-09 13:42:36 -0400

    added documentation for AES

--- src/main/java/AES.java
+++ src/main/java/AES.java
@@ -13,11 +13,32 @@ import java.security.NoSuchAlgorithmException;
 
 public class AES {
 
+    /*
+    SYMMETRIC ALGORITHM ADVANCED ENCRYPTION STANDARD IS USED FOR
+    SECURITY BY ENCRYPTING AND DECRYPTING A PASSWORD FOR CONNECTION
+    AND TO AVOID UNWANTED ACCESS
+     */
+
     private SecretKeySpec secretKey;
+
+    /**
+     *
+     * @param pWord generate a secret key that will be used for encryption and decryption
+     */
 
     private void setKey(byte [] pWord){
         secretKey = new SecretKeySpec(pWord, Constants.ALGORITHM);
     }
+
+    /**
+     * Uses the java built in AES library to encrypt a password that has been set by the
+     * server which is 16 characters or less
+     * @param packetBytes is the incoming byte array that is being encrypted and
+     *                    does the specified transformation
+     * @param pWord is the password byte array that is being encrypted
+     * @return
+     */
+
 
     public byte [] encrypt(byte [] packetBytes, byte[] pWord){
         ByteBuffer buffer = ByteBuffer.allocate(Constants.KEY_SIZE);
@@ -35,6 +56,14 @@ public class AES {
         }
         return null;
     }
+
+    /**client
+     * Decryption of the encrypted password
+     * @param packetBytes the encrypted packet bytes are decrypted which is essentially
+     *                    the encryption process done backwards
+     * @param pWord the incoming password that has been encrypted is decrypted
+     * @return
+     */
 
     public byte [] decrypt(byte [] packetBytes, byte [] pWord){
         ByteBuffer buffer = ByteBuffer.allocate(Constants.KEY_SIZE);
